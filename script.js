06d00d5816dd01a4daac0db97913b5df5bae67f8
abhipatel1369 <72403818+abhipatel1369@users.noreply.github.com>
2023-09-11 19:33:14 +0530

    initial card

---
+++ script.js
@@ -0,0 +1,41 @@
+const card = document.getElementById('myCard');
+let isDragging = false;
+let initialX, currentX, deltaX, rotationAngle = 0;
+
+card.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    initialX = e.clientX;
+});
+
+card.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    initialX = e.touches[0].clientX;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    currentX = e.clientX;
+    deltaX = currentX - initialX;
+
+    rotationAngle += deltaX / 5;
+    card.style.transform = `rotateY(${rotationAngle}deg)`;
+});
+
+document.addEventListener('touchmove', (e) => {
+    if (!isDragging) return;
+    currentX = e.touches[0].clientX;
+    deltaX = currentX - initialX;
+
+    rotationAngle += deltaX / 5;
+    card.style.transform = `rotateY(${rotationAngle}deg)`;
+});
+
+document.addEventListener('mouseup', () => {
+    if (!isDragging) return;
+    isDragging = false;
+});
+
+document.addEventListener('touchend', () => {
+    if (!isDragging) return;
+    isDragging = false;
+});
